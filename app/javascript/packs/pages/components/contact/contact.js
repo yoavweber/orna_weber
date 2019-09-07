96542950cba5089579d193bf5c6b301465572a4a
yoav weber <yoav.weber@code.berlin>
2019-09-07 10:53:55 +0200

    fixing API key issue

--- app/javascript/packs/pages/components/contact/contact.js
+++ app/javascript/packs/pages/components/contact/contact.js
@@ -1,29 +1,46 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
+import marker from './google_marker.png'
 
+import './contact.scss';
+
+
+require('dotenv').config();
+
+console.log(process.env.GOOGLE_API)
+
+
+const AnyReactComponent = () => <div className="marker"style={{ height: '50px', width: '50%', float: 'left', position:'absolute'}}><img src={marker} /></div> ;
+
+
+const LOCATION  = {
+  lat: 32.030639,
+  lng: 34.877925
+}
 
 const Contact = () =>{
  const defaultProps = {
-    center: {
-      lat: 59.95,
-      lng: 30.33
-    },
-    zoom: 11
+    center: LOCATION,
+    zoom: 16
   };
   return(
-    <div style={{ height: '100vh', width: '100%' }}>
+    <div style={{ height: '50vh', width: '50%', float: 'left'}}>
         <GoogleMapReact
-          bootstrapURLKeys={{ key: ''}}
+          bootstrapURLKeys={{ key: `${process.env.GOOGLE_API}`}}
           defaultCenter={defaultProps.center}
           defaultZoom={defaultProps.zoom}
-        />
+        >
+           <AnyReactComponent
+            lat={32.030639}
+            lng={34.877925}
+            text="My Marker"
+          />
+        </GoogleMapReact>
     </div>
     )
     
 }
 
-
-// 2VJH+37 Yehud, Israel
 
 
 
