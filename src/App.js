9aa3c6a2d34e78af4759bdbe5be2060ff40c8893
yoavweber <yoav.weber@code.berlin>
2020-03-19 22:13:17 +0100

    moving files around

--- src/App.js
+++ src/App.js
@@ -1,44 +1,44 @@
-import React from 'react';
-// import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+// import React from 'react';
+// // import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
-import Nav from './layout/common/nav/nav';
-import Footer from './layout/common/footer/footer';
+// import Nav from './layout/common/nav/nav';
+// import Footer from './layout/common/footer/footer';
 
-import about from './pages/components/about/about';
-import Home from './pages/components/home/home';
-import Treatment from './pages/components/treatments/treatmentsTamplate';
-import booking from './pages/components/booking/booking';
-import Contact from './pages/components/contact/contact';
-import { useStaticQuery, Link, graphql } from "gatsby"
-
-
-import './App.css';
+// import about from './pages/components/about/about';
+// import Home from './pages/components/home/home';
+// import Treatment from './pages/components/treatments/treatmentsTamplate';
+// import booking from './pages/components/booking/booking';
+// import Contact from './pages/components/contact/contact';
+// import { useStaticQuery, Link, graphql } from "gatsby"
 
 
+// import './App.css';
 
 
-function App() {
-  return (
-    <div className="app">
-      <Nav />
-        {/* <Link path="/" exact component={Home} /> */}
-        <Home />
-      <Footer />
-    </div>  
-    // <Router>
-    //   <div className="app">
-    //     <Nav />
-    //     <Switch>
-    //       <Route path="/" exact component={Home} />
-    //       <Route path="/about" component={about}/>
-    //       <Route path="/treatments" component={Treatment}/>
-    //       <Route path="/booking" component={booking}/>
-    //       <Route path="/contact" component={Contact}/>
-    //     </Switch>
-    //     <Footer />
-    //   </div>
-    // </Router>
-  );
-}
 
-export default App;
+
+// function App() {
+//   return (
+//     <div className="app">
+//       <Nav />
+//         {/* <Link path="/" exact component={Home} /> */}
+//         <Home />
+//       <Footer />
+//     </div>  
+//     // <Router>
+//     //   <div className="app">
+//     //     <Nav />
+//     //     <Switch>
+//     //       <Route path="/" exact component={Home} />
+//     //       <Route path="/about" component={about}/>
+//     //       <Route path="/treatments" component={Treatment}/>
+//     //       <Route path="/booking" component={booking}/>
+//     //       <Route path="/contact" component={Contact}/>
+//     //     </Switch>
+//     //     <Footer />
+//     //   </div>
+//     // </Router>
+//   );
+// }
+
+// export default App;
