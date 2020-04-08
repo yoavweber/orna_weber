d2b3cc06991c366931664950bfbeeab74a98cdbe
yoavweber <yoav.weber@code.berlin>
2020-04-08 22:33:08 +0200

    adding burger to nav WIP

--- src/layout/common/nav/nav.js
+++ src/layout/common/nav/nav.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import {Link} from 'gatsby';
-import logo from '../.././assets/logo.png';
-import './nav.scss';
+import React, { useState } from 'react'
+import { Link } from 'gatsby'
+import logo from '../.././assets/logo.png'
+import styles from './nav.module.scss'
 
-
-
-function Nav({children}){
-    return(
-        <div>
-            <nav>
-                <img src={logo} />
-                <ul className="nav-links">
-                    <Link to= '/'>
-                        <li>ראשי</li>
-                    </Link>
-                    <Link to='hairRemoval'>
-                        <li>טיפולים</li>
-                    </Link>
-                    <Link to='booking'>
-                        <li>קביעת תור</li>
-                    </Link>
-                    <Link to='about'>
-                        <li>עלינו</li>
-                    </Link>
-                    <Link to='contact'>
-                        <li>צור קשר</li>
-                    </Link>
-                </ul>
-            </nav>
-            {children}
-        </div>
+function Nav({ children }) {
+  const Burger = () => {
+    return (
+      <button className={styles.burgerWrapper}>
+        <div />
+        <div />
+        <div />
+      </button>
     )
+  }
+
+  return (
+    <div>
+      <nav>
+        <Burger />
+        <img src={logo} />
+        <ul className={styles.navLinks}>
+          <Link to="/">
+            <li>ראשי</li>
+          </Link>
+          <Link to="hairRemoval">
+            <li>טיפולים</li>
+          </Link>
+          <Link to="booking">
+            <li>קביעת תור</li>
+          </Link>
+          <Link to="about">
+            <li>עלינו</li>
+          </Link>
+          <Link to="contact">
+            <li>צור קשר</li>
+          </Link>
+        </ul>
+      </nav>
+      {children}
+    </div>
+  )
 }
 
-
-export default Nav;
+export default Nav
