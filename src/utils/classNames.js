31ac1a37487acd8e2795bff8fc924e26d78052e2
yoavweber <yoav.weber@code.berlin>
2020-05-03 18:04:20 +0200

    handling objects in classnames utils

--- src/utils/classNames.js
+++ src/utils/classNames.js
@@ -1,4 +1,25 @@
-export const classNames = (...classes) => {
-    const result = classes.filter(className => className !== undefined)
-    return result.join(' ')
-  }
+// export const classNames = (...classes) => {
+//     const result = classes.filter(className => className !== undefined)
+//     // console.log(result.join(' '))
+//     return result.join(' ')
+//   }
+
+
+
+  export const classNames = (...classes) => {
+      const result = classes.reduce((accumulator,className) => {
+        if(typeof className !== 'undefined'){
+          if(typeof className === 'object' && Object.values(className)[0] === true){
+            const test = Object.keys(className)[0]
+            accumulator.push(test)
+          }else if(typeof className === 'string'){
+            accumulator.push(className)
+          }
+          
+        }
+        return accumulator 
+      },[])
+
+      return result.join(' ')
+    }
+  
