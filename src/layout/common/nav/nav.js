be6237cf9fe7ceaf1688e823c61e7d76bba0b11b
yoavweber <yoav.weber@code.berlin>
2020-05-05 22:21:14 +0200

    making navbar responsive

--- src/layout/common/nav/nav.js
+++ src/layout/common/nav/nav.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Link } from 'gatsby'
 import AOS from 'aos'
-import 'aos/dist/aos.css';
+import 'aos/dist/aos.css'
 import logo from '../.././assets/logo.png'
 import styles from './nav.module.scss'
 import { classNames } from '../../../utils/classNames'
@@ -11,14 +11,14 @@ let aos
 
 function Nav({ children }) {
   const [scrolled, setScrolled] = useState(false)
+  const [menu, setMenu] = useState(false)
 
   useEffect(() => {
     console.log('when its called?')
     if (aos) {
-      console.log(aos,'from if/')
+      console.log(aos.init(), 'from if/')
       // aos.refresh()
     } else {
-      console.log(aos,'from else')
       aos = AOS
       aos.init({
         initClassName: false,
@@ -43,15 +43,14 @@ function Nav({ children }) {
     }
   })
 
-  
-  
-
   const Burger = () => {
     return (
-      <button className={styles.burgerWrapper}>
-        <div />
-        <div />
-        <div />
+      <button className={styles.burgerWrapper} onClick={() => setMenu(!menu)}>
+        <svg width="32" height="16" viewBox="0 0 32 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect width="32" height="2" fill="black" />
+          <rect y="7" width="32" height="2" fill="black" />
+          <rect y="14" width="32" height="2" fill="black" />
+        </svg>
       </button>
     )
   }
@@ -59,36 +58,63 @@ function Nav({ children }) {
   const AOSLink = (props, children) => {
     return (
       <>
-        <Link to={props.to} data-aos-once="true"  data-aos="zoom-in" data-aos-duration="200" data-aos-delay={props.delay}>
+        <Link
+          to={props.to}
+          data-aos-once="true"
+          data-aos="zoom-in"
+          data-aos-duration="200"
+          data-aos-delay={props.delay}
+        >
           {props.children}
         </Link>
       </>
     )
   }
+  let responsiveNav = window.innerWidth < 850
 
   return (
     <div className={styles.layoutWrapper}>
-      <nav data-active={scrolled}>
-        <div className={styles.wrapper}>
-          <Burger />
-          <AOSLink to="/">
-            <img src={logo} width="100px" alt="Orna_logo" />
-          </AOSLink>
-          <AOSLink to="/" delay="200">ראשי</AOSLink>
-          <AOSLink to="hair" delay="400">
-            טיפולים
-          </AOSLink>
-          <AOSLink to="booking" delay="600">
-            קביעת תור
-          </AOSLink>
-          <AOSLink to="about" delay="600">
-            עלינו
-          </AOSLink>
-          <AOSLink to="contact" delay="800">
-            צרי קשר
-          </AOSLink>
-        </div>
-      </nav>
+      {console.log(responsiveNav)}
+      {responsiveNav ? (
+        <nav data-scroll={scrolled} className={styles.responsiveNav} data-menu={menu}>
+          <div className={styles.layout}>
+            <Burger />
+            <AOSLink to="/" className="hi">
+              <img src={logo} width="100px" alt="Orna_logo" />
+            </AOSLink>
+          </div>
+          <div className={styles.content}>
+            <AOSLink to="/">ראשי</AOSLink>
+            <AOSLink to="hair">טיפולים</AOSLink>
+            <AOSLink to="booking">קביעת תור</AOSLink>
+            <AOSLink to="about">עלינו</AOSLink>
+            <AOSLink to="contact">צרי קשר</AOSLink>
+          </div>
+        </nav>
+      ) : (
+        <nav data-scroll={scrolled} className={styles.desktopNav}>
+          <div className={styles.wrapper} data-active={menu}>
+            <AOSLink to="/">
+              <img src={logo} width="100px" alt="Orna_logo" />
+            </AOSLink>
+            <AOSLink to="/" delay="200">
+              ראשי
+            </AOSLink>
+            <AOSLink to="hair" delay="400">
+              טיפולים
+            </AOSLink>
+            <AOSLink to="booking" delay="600">
+              קביעת תור
+            </AOSLink>
+            <AOSLink to="about" delay="600">
+              עלינו
+            </AOSLink>
+            <AOSLink to="contact" delay="800">
+              צרי קשר
+            </AOSLink>
+          </div>
+        </nav>
+      )}
       {children}
     </div>
   )
